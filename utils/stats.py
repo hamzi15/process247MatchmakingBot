bf2a288c45bdf9991f072664056ce3a26864e974
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2021-10-18 15:10:08 +0500

    Update stats.py

--- utils/stats.py
+++ utils/stats.py
@@ -12,14 +12,17 @@ else:
     with open("config.json") as file:
         config = json.load(file)
 
-#We will recieve the entire red team and blue team dictionaries with memberObjs
+
+# We will recieve the entire red team and blue team dictionaries with memberObjs
 
 class Stats:
     lol_roles = ['Top', 'Jungle', 'Mid', 'Adc', 'Support']
+
     def __init__(self):
         pass
+
     @staticmethod
-    def get_stats(red,blue):
+    def get_stats(red, blue):
         red_members = []
         blue_members = []
         for role in Stats.lol_roles:
@@ -34,29 +37,32 @@ class Stats:
 
         matchIdlst = []
         for puuid in puuid_dict:
-            response = Stats.fetch_matchIds(puuid)
+            response = Stats.fetch_match_ids(puuid)
             if response:
                 matchIdlst.append(response[0])
 
-        #Getting most common match id
+        # Getting most common match id
         matchId = Stats.Most_Common(matchIdlst)
 
-        response = Stats.fetch_matchData(matchId)
+        response = Stats.fetch_match_data(matchId)
         metadata_participants = response.json()['metadata']['participants']
         info_participants = response.json()['info']['participants']
-        #{discordid: [Champion_name,win,kills,deaths,assists,CreepScore????,doublekills,triplekills,quadrakills,pentakills,totalDamageDealt,totalDamageTaken]}
+        # {discordid: [Champion_name,win,kills,deaths,assists,CreepScore????,doublekills,triplekills,quadrakills,pentakills,totalDamageDealt,totalDamageTaken]}
         stats = {}
         for index in range(len(info_participants)):
             participant = info_participants[index]
             puuid = metadata_participants[index]
-            discordid = puuid_dict[puuid].id #discordid
-            #Champion_name,kills,deaths,assists,doublekills,triplekills,quadrakills,pentakills
-            #CREEPSCORE IS AN ISSUE
-            creepScorePerMin= participant["totalMinionsKilled"]/(participant["timePlayed"]/60)
-            statslst = [participant["championName"],participant["win"],participant["kills"],participant["deaths"],participant["assists"],
-                        creepScorePerMin,participant["doubleKills"],participant["tripleKills"],participant["quadraKills"],
-                        participant["pentaKills"],participant["totalDamageDealt"],participant["totalDamageTaken"]]
+            discordid = puuid_dict[puuid].id  # discordid
+            # win,kills,deaths,assists,doublekills,triplekills,quadrakills,pentakills
+            creepScorePerMin = participant["totalMinionsKilled"] / (participant["timePlayed"] / 60)
+            statslst = {"win": participant["win"], "kills": participant["kills"], "deaths": participant["deaths"],
+                        "assists": participant["assists"],
+                        "totalMinionsKilled": participant["totalMinionsKilled"], "timepPlayed":(participant["timePlayed"]/60),
+                        "tripleKills": participant["tripleKills"],"quadraKills": participant["quadraKills"],
+                        "pentaKills": participant["pentaKills"], "totalDamageDealt": participant["totalDamageDealt"],
+                        "totalDamageTaken": participant["totalDamageTaken"]}
             stats[discordid] = statslst
+        return stats
 
     @staticmethod
     def Most_Common(lst):
@@ -69,21 +75,23 @@ class Stats:
         return max(lst, key=data.get)
 
     @staticmethod
-    def fetch_matchData(matchId):
+    def fetch_match_data(matchId):
         API_KEY = config['RIOT_API_KEY']
         region = "americas"
         for i in range(3):
-            response = requests.get(f"https://{region}.api.riotgames.com/lol/match/v5/matches/{matchId}?api_key={API_KEY}")
+            response = requests.get(
+                f"https://{region}.api.riotgames.com/lol/match/v5/matches/{matchId}?api_key={API_KEY}")
             if response.status_code == 200:
                 return response
 
     @staticmethod
-    def fetch_matchIds(puuid):
+    def fetch_match_ids(puuid):
         API_KEY = config['RIOT_API_KEY']
         region = "americas"
         no_of_matches = 1
         for i in range(3):
-            response = requests.get(f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={no_of_matches}&api_key={API_KEY}")
+            response = requests.get(
+                f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={no_of_matches}&api_key={API_KEY}")
             if response.status_code == 200:
                 return response.json()
 
@@ -98,7 +106,8 @@ class Stats:
 
         league_region = 'na1'  # hardcoded
         for i in range(3):
-            response = requests.get(f"https://{league_region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{league_id}?api_key={API_KEY}")
+            response = requests.get(
+                f"https://{league_region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{league_id}?api_key={API_KEY}")
             if response.status_code == 200:
                 return response.json()['puuid']
             elif response.status_code == 404:
@@ -110,7 +119,3 @@ class Stats:
             elif response.status_code in [400, 401, 403, 405, 500, 502]:
                 print('API Error')
                 break
-
-
-
-
