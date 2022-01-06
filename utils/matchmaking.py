9fbb38a51743306ec37446ecddeabd703e85b884
Hamza Asaad <66818162+hamzi15@users.noreply.github.com>
2022-01-06 18:14:25 +0500

    Update matchmaking.py

--- utils/matchmaking.py
+++ utils/matchmaking.py
@@ -154,8 +154,8 @@ class MatchMaking:
     @staticmethod
     def player_valuation(tier, rank, wins, loses, lp):
         totalMatches = wins + loses
-        if totalMatches < 50:
-            return False
+        #if totalMatches < 50:
+        #    return False
         WinRate = (wins / totalMatches) * 100  # Percentage winrate
         mmr = int
         print('inside rank_value')
